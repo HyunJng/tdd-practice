94bf7afd2ff9d7b0f0f1671684647edf8380d9d8
HyunJng <khjung1654@gmail.com>
2025-06-15 05:16:59 +0900

    init

---
+++ src/main/java/org/example/demo/TddTrainApplication.java
@@ -0,0 +1,20 @@
+package org.example.demo;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "TDD Practice Project",
+        version = "v1",
+        description = "TDD 연습 토이 프로젝트"
+))
+public class TddTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddTrainApplication.class, args);
+    }
+
+}
